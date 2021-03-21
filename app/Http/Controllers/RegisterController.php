f5e1a4c153d5ab37a6c720a599b804210d1f08d5
Hatch <hatch@pop-os.localdomain>
2021-03-21 22:22:03 +0100

    Enabled user registration, login and logout

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function __construct() {
+        $this->middleware(['guest']);
+    }
+
+    public function index() {
+        return view('auth.register');
+    }
+
+    public function store(Request $request) {
+        // dd($request, $request->email); request has parameters which is array - post[<name>]
+        
+        $this->validate($request, [
+            'name' =>  ['required', 'max:255'],
+            'username' => ['required', 'max:255'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'confirmed'], //looks for any names in $request->parameters with '_confirmed'
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        
+        auth()->attempt($request->only('email', 'password'));
+
+        return redirect()->route('home');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +17,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+
+// rute
+
+
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
+
+Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
